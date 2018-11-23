5247be14f70021c14790d73a510cf713be461dcc
niyomaof <susun191@gmail.com>
2018-11-24 00:49:44 +0700

    Commit 0.49

--- menu.php
+++ menu.php
@@ -8,7 +8,7 @@
         <ul class="navbar-nav mr-auto">
 
             <li class="nav-item">
-                <a class="nav-link" href="">Home</a>
+                <a class="nav-link" href="?menu=home">Home</a>
             </li>
 
             <li class="nav-item dropdown">
